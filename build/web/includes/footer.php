3a8a271850e5c4b29e05692ff431466b7ab5b091
Huyền 03719 <huyendtnph03719@fpt.edu.vn>
2017-10-26 10:31:12 +0700

    ttu
    tutr

---
+++ build/web/includes/footer.php
@@ -0,0 +1,64 @@
+<div id="marginfooter">
+<div class="muc">
+
+<span>Liên hệ</span>
+
+
+
+
+
+<p>Địa chỉ:27 Núi Thành - Hải Châu - Đà Nẵng</p>
+<p >Điện thoại:01205307920</ơ>
+<p >Fax:</p>
+<p >Email:rynphan6@gmail.com</p>
+
+</div>
+
+
+<div class="muc">
+<span>Menu</span><div id="menufooter">
+
+<ul>
+<li><a href="?mod=home">Trang chủ</a></li>
+<li><a href="?mod=gioithieu">Giới thiệu</a></li>
+<li><a href="?mod=products">Sản Phẩm</a></li>
+<li><a href="?mod=lienhe">Liên hệ</a></li>
+<li><a href="?mod=mali">Bản đồ</a></li>
+</ul>
+
+</div></div>
+<div class="muc">
+<span>Danh mục</span><div id="danhmucfooter">
+<ul >
+	<li><a href="?mod=products&type=1">Điện thoại di động</a>
+	
+	  </li>
+		<li><a href="?mod=products&type=2">Máy tính bảng</a>
+	
+	  </li>
+	  <li><a href="?mod=products&type=3">Máy tính xách tay</a>
+	
+	  </li>
+	  <li><a href="?mod=products&type=5">Phụ kiện điện thoại</a>
+	
+	  </li>
+	</ul>
+
+
+
+</div></div>
+<div class="muc">
+<span>Tìm kiếm</span>
+<div id="search">
+
+<label>Search</label>
+<input type="text" name="txtSearchText"  id="text" value="" />
+<input type="submit" name="btnSearch" value="Tìm Kiếm" id="button" />
+
+</div></div>
+<div class="clear"></div>
+</div>
+
+<div class="thietke">
+		Thiết kế bởi: <span> Phan Châu Thành </span>
+		</div>
---
+++ build/web/modules/map/map.php
@@ -0,0 +1,6 @@
+<h3>Bản Đồ</h3>
+
+	 <iframe width="700" height="400"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=vi&amp;geocode=&amp;q=27+N%C3%BAi+Th%C3%A0nh,+H%E1%BA%A3i+Ch%C3%A2u,+%C4%90%C3%A0+N%E1%BA%B5ng&amp;aq=0&amp;oq=27&amp;sll=16.052577,108.213186&amp;sspn=0.006279,0.010504&amp;gl=vn&amp;ie=UTF8&amp;hq=&amp;hnear=27+N%C3%BAi+Th%C3%A0nh,+H%E1%BA%A3i+Ch%C3%A2u,+%C4%90%C3%A0+N%E1%BA%B5ng&amp;ll=16.052577,108.213186&amp;spn=0.006279,0.010504&amp;t=m&amp;z=14&amp;output=embed"></iframe>
+					    <br />
+				      <small>Xem <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=vi&amp;geocode=&amp;q=27+N%C3%BAi+Th%C3%A0nh,+H%E1%BA%A3i+Ch%C3%A2u,+%C4%90%C3%A0+N%E1%BA%B5ng&amp;aq=0&amp;oq=27&amp;sll=16.052577,108.213186&amp;sspn=0.006279,0.010504&amp;gl=vn&amp;ie=UTF8&amp;hq=&amp;hnear=27+N%C3%BAi+Th%C3%A0nh,+H%E1%BA%A3i+Ch%C3%A2u,+%C4%90%C3%A0+N%E1%BA%B5ng&amp;ll=16.052577,108.213186&amp;spn=0.006279,0.010504&amp;t=m&amp;z=14" style="color:#0000FF;text-align:left">TP Đà Nẵng - Việt Nam</a> ở bản đồ lớn hơn</small><br />
+				
